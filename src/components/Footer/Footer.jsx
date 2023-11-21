506ada5dfab7047ef3ef528c2127fa7818431a63
Muskan Tejwani <muskantejwani445@gmail.com>
2023-11-21 21:33:53 +0500

    react page

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+import { 
+  Box, 
+  Stack, 
+  styled, 
+  Typography,
+} from '@mui/material'
+import Link from '@mui/material/Link';
+import FooterTitle from './FooterTitle'
+import FooterLink from './FooterLink'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedIcon from '@mui/icons-material/LinkedIn';
+const Footer = () => {
+
+  const StackColumn = styled(Stack) (() => ({
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 1,
+    gap: 8,
+    textAlign: 'center',
+  }));
+
+  const BoxRow = styled(Box) (({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: '#ededed',
+    flex: 1,
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      gap: 30,
+    }
+  }));
+
+  return (
+    
+    <BoxRow 
+    component = 'footer'
+    sx={{
+      py: 4,
+      px: 2,
+    }}
+    >
+      <StackColumn>
+        <FooterTitle text={'address'} />
+        <FooterLink 
+        text={'Neque porro quisquam est qui dolorem'} 
+        />
+        <FooterLink 
+        text={'99999999999999'} 
+        />
+        <FooterLink 
+        text={'info@example.com'} 
+        />
+      </StackColumn>
+      
+      <StackColumn>
+        <FooterTitle text={'our services'} />
+        <FooterLink text={'buy property'} />
+        <FooterLink text={'sell property'} />
+        <FooterLink text={'rent property'} />
+        <FooterLink text={'build property'} />
+      </StackColumn>
+      <StackColumn>
+        <FooterTitle text={'our company'} />
+        <FooterLink text={'reporting'} />
+        <FooterLink text={'get in touch'} />
+        <FooterLink text={'management'} />
+      </StackColumn>
+
+      <StackColumn>
+        <FooterTitle text={'MSales'} />
+        <Stack 
+        direction='row' 
+        width= '70px'
+        maxWidth='100%'
+        justifyContent='space-between'
+        >
+          <Link href="#" variant="body2" 
+          sx={{
+            color: '#414141',
+            "&:hover": {
+              color: '#1c2859',
+            }
+          }}
+          >
+            <LinkedIcon />
+          </Link> 
+          <Link href="#"variant="body2" 
+          sx={{
+            color: '#414141',
+            "&:hover": {
+              color: '#1c2859',
+            }
+          }}
+          >
+            <FacebookIcon />
+            
+          </Link> 
+        </Stack>
+        <Typography 
+        variant='caption'
+        component='p' 
+        >
+          &copy; 2023 MSales.
+        </Typography>
+      </StackColumn>
+    </BoxRow>
+  )
+}
+
+export default Footer
